fca1a1cf0850ff84b5469e92efc9c031254eda24
Mariya Ivanova <m80ivanova@gmail.com>
2021-02-27 21:00:35 -0600

    Animations to Home Page

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,45 @@ $(function() {
         }
     });
 });
+ 
+//The following is for adding different kinds of animations for the home page
+window.sr = ScrollReveal();
+sr.reveal(".leftDr", {
+duration: 2000,
+origin: "left",
+distance: "300px"
+});
+sr.reveal(".rightDr", {
+    duration: 2000,
+    origin: "right",
+    distance: "300px"
+});
+sr.reveal(".topSlide", {
+    duration: 2000,
+    origin: "top",
+    distance: "100px"
+});
+sr.reveal(".slideLeft", {
+    duration: 2000,
+    delay: 1000,
+    origin: "right",
+    distance: "100px"
+});
+sr.reveal(".slideRight", {
+    duration: 2000,
+    origin: "left",
+    distance: "300px"
+});
+sr.reveal(".footerCards", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "200px"
+});
+sr.reveal(".easeIn", {
+    duration: 2000,
+    easing: "ease-in",
+});
+sr.reveal(".scaled", {
+    duration: 1000,
+    scale: 0.85
+})
